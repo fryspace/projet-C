352581e69d3b99d2aa84b6a93b4aeaa3842159f9
Aituglo <contact@aituglo.com>
2021-05-09 12:53:04 +0200

    Merge evrything

--- tests/lines.c
+++ tests/lines.c
@@ -38,7 +38,7 @@ void test_button(ei_surface_t surface, ei_rect_t* clipper)
 
     ei_linked_point_t *pts = ei_rounded_frame(rect, 50, 0, 10);
 
-    ei_draw_polygon(surface, pts, color, clipper);
+    //ei_draw_polygon(surface, pts, color, clipper);
 }
 
 /* test_line --
@@ -194,11 +194,11 @@ int main(int argc, char** argv)
 	ei_fill		(main_window, &white, clipper_ptr);
 
 	/* Draw polylines. */
-    test_button(main_window, clipper_ptr);
+    //test_button(main_window, clipper_ptr);
 	//test_line(main_window, clipper_ptr);
     //test_heart(main_window, clipper_ptr);
-	//test_octogone	(main_window, clipper_ptr);
-	//test_square	(main_window, clipper_ptr);
+	test_octogone	(main_window, clipper_ptr);
+	test_square	(main_window, clipper_ptr);
 	//test_dot	(main_window, clipper_ptr);
 	
 	/* Unlock and update the surface. */
